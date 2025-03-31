8ee775bb0064f269b35c777fb8a583548553e76e
VITOR MIGUEL RODRIGUES CEZARIO <25132565@CFP127ALUNO.LOCAL>
2025-03-31 16:53:03 -0300

    Nova Tabuada

---
+++ src/br/dev/vitor/calculadoratabuada/Main.java
@@ -0,0 +1,14 @@
+package br.dev.vitor.calculadoratabuada;
+
+import br.dev.vitor.calculadoratabuada.gui.TelaTabuada;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		TelaTabuada tela = new TelaTabuada();
+		tela.criarTela();
+		
+	}
+
+}
