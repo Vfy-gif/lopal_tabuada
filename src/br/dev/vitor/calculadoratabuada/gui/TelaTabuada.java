1e46c21db993ebf3f62663a8a9a45ee083762f7e
VITOR MIGUEL RODRIGUES CEZARIO <25132565@CFP127ALUNO.LOCAL>
2025-04-07 17:00:43 -0300

    Avanços na Tela

--- src/br/dev/vitor/calculadoratabuada/gui/TelaTabuada.java
+++ src/br/dev/vitor/calculadoratabuada/gui/TelaTabuada.java
@@ -2,12 +2,27 @@ package br.dev.vitor.calculadoratabuada.gui;
 
 import java.awt.Dimension;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
 public class TelaTabuada {
 
+	private JLabel labelMultiplicando;
+	private JTextField txtMultiplicando;
+	private JLabel labelMinMultiplicador;
+	private JTextField txtMinMultiplicador;
+	private JLabel labelMaxMultiplicador;
+	private JTextField txtMaxMultiplicador;
+	private JButton btnCalcular;
+	private JButton btnLimpar;
+	private JLabel labelResultado;
+	private JScrollPane scrollTabuada;
+	private JList listaTabuada;
+	
 	public void criarTela() {
 		
 		JFrame tela = new JFrame();
@@ -15,26 +30,71 @@ public class TelaTabuada {
 		// Definir o tamanho da tela
 		// através de um objeto Dimension
 		Dimension tamanho = new Dimension();
-		tamanho.setSize(400, 700);
+		tamanho.setSize(335, 700);
 		tela.setSize(tamanho);
 		tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		tela.setTitle("Tabuada");
 		tela.setLayout(null);
 		tela.setLocationRelativeTo(null);
+		tela.setResizable(false);
 		
-		// Criar um JLabel
-		JLabel labelMultiplicando = new JLabel();
+		// Criar um JLabel e um JTextField para o multiplicando
+		labelMultiplicando = new JLabel();
 		labelMultiplicando.setText("Valor do multiplicando:");
 		labelMultiplicando.setBounds(50, 40, 150, 30);
 		
-		// Criar um TextField
+		txtMultiplicando = new JTextField();
+		txtMultiplicando.setBounds(210, 40, 60, 30);
 		
-		JTextField txtMultiplicando = new JTextField();
-		txtMultiplicando.setBounds(202, 40, 60, 30);
+		// Criar um JLabel e um JTextField para o multiplicando
+		labelMinMultiplicador = new JLabel();
+		labelMinMultiplicador.setText("Mínimo multiplicador");
+		labelMinMultiplicador.setBounds(50, 80, 150, 30);
 		
+		txtMinMultiplicador = new JTextField();
+		txtMinMultiplicador.setBounds(210, 80, 60, 30);
+		
+		labelMaxMultiplicador = new JLabel();
+		labelMaxMultiplicador.setText("Máximo multiplicador");
+		labelMaxMultiplicador.setBounds(50, 120, 150, 30);
+		
+		txtMaxMultiplicador = new JTextField();
+		txtMaxMultiplicador.setBounds(210, 120, 60, 30);
+		
+		//Criando os botões da tela
+		btnCalcular = new JButton();
+		btnCalcular.setText("Calcular");
+		btnCalcular.setBounds(50, 160, 105, 30);
+		
+		btnLimpar = new JButton();
+		btnLimpar.setText("Limpar");
+		btnLimpar.setBounds(165, 160, 105, 30);
+		
+		// Label do resultado
+		labelResultado = new JLabel();
+		labelResultado.setText("Resultado: ");
+		labelResultado.setBounds(50, 200, 200, 30);
+		
+		// Criando a lista que exbirá a tabuada
+		listaTabuada = new JList();
+		
+		// Criando o painel de rolagem para a lista
+		scrollTabuada = new JScrollPane(listaTabuada);
+		scrollTabuada.setBounds(50, 240, 220, 380);
+		
+		// Adicionando os componentes ao painel de conteúdo
 		tela.getContentPane().add(labelMultiplicando);
 		tela.getContentPane().add(txtMultiplicando);
+		tela.getContentPane().add(labelMinMultiplicador);
+		tela.getContentPane().add(txtMinMultiplicador);
+		tela.getContentPane().add(labelMaxMultiplicador);
+		tela.getContentPane().add(txtMaxMultiplicador);
+		tela.getContentPane().add(btnCalcular);
+		tela.getContentPane().add(btnLimpar);
+		tela.getContentPane().add(labelResultado);
+		tela.getContentPane().add(scrollTabuada);
 		
+		// Tornar a tela vísivel deverá ser a última linha deste método
 		tela.setVisible(true);
 		
 	}
--- src/br/dev/vitor/calculadoratabuada/model/Tabuada.java
+++ src/br/dev/vitor/calculadoratabuada/model/Tabuada.java
@@ -38,6 +38,10 @@ public class Tabuada {
 			menorMultiplicador = temp;
 		}
 		
+		int tamanhoVetor = (int) (maiorMultiplicador - menorMultiplicador + 1);
+		
+		String tabuada[] = new String[tamanhoVetor];
+		
 		while(menorMultiplicador <= maiorMultiplicador) {
 			double produto = multiplicando * menorMultiplicador;
 			System.out.printf("%s x %s = %s\n", multiplicando, menorMultiplicador, produto);
